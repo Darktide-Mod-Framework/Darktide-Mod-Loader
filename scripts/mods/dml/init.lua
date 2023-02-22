a36e6f4adbd537be882b511ec2e95b10944ece58
Lucas Schwiderski <lucas@lschwiderski.de>
2023-02-22 09:55:27 +0100

    feat: Implement mod loader
    Co-authored-by: Aussiemon <mattrohrlach+github@gmail.com>

--- scripts/mods/dml/init.lua
+++ scripts/mods/dml/init.lua
@@ -13,4 +13,20 @@ dofile("scripts/mods/dml/require")
 dofile("scripts/mods/dml/class")
 dofile("scripts/mods/dml/hook")
 
+function loader:init(boot_gui, mod_data)
+    local ModLoader = dofile("scripts/mods/dml/mod_loader")
+    local mod_loader = ModLoader:init(boot_gui, mod_data)
+    self._mod_loader = mod_loader
+
+    Mods.hook.set(StateGame, "update", function(func, dt, ...)
+        mod_loader:update(dt)
+        return func(dt, ...)
+    end)
+
+end
+
+function loader:update(dt)
+    self._mod_loader:update(dt)
+end
+
 return loader
